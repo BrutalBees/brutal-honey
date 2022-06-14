0756ed354ef11f01e1717c5c8a3236d3d7e23d24
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-06-14 17:48:05 -0400

    feat: add ProductIndex and ProductIndexItem  component

---
+++ client/store/products.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const SET_PRODUCTS = "SET_PRODUCTS";
+
+const setProducts = (products) => {
+  return {
+    type: SET_PRODUCTS,
+    products
+  }
+};
+
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    const {data: products} = await axios.get('/api/products');
+    dispatch(setProducts(products));
+  }
+};
+
+// PRODUCTS REDUCER
+export default (state = [], action) => {
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return action.products;
+    default:
+      return state;
+  }
+};
