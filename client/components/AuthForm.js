fadd6e77d88f9e13d37222071ad41385ed73ee08
Katie Assoian <ktassoian@gmail.com>
2022-06-17 15:46:09 -0400

    add ternary to authform

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { authenticate } from '../store';
-import history from '../history';
 
 /**
  * COMPONENT
@@ -10,39 +9,46 @@ const AuthForm = (props) => {
   const { name, displayName, handleSubmit, error } = props;
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
+    <form onSubmit={handleSubmit} name={name}>
+      {Login ? (
         <div>
           <label htmlFor="email">
             <small>Email</small>
           </label>
           <input name="email" type="text" />
-        </div>
-        <div>
+
           <label htmlFor="password">
             <small>Password</small>
           </label>
           <input name="password" type="password" />
         </div>
+      ) : (
         <div>
           <label htmlFor="firstName">
             <small>First Name</small>
           </label>
           <input name="firstName" type="text" />
-        </div>
-        <div>
+
           <label htmlFor="lastName">
             <small>Last Name</small>
           </label>
           <input name="lastName" type="text" />
-        </div>
 
-        <div>
-          <button type="submit">{displayName}</button>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
         </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-    </div>
+      )}
+      <button type="submit">{displayName}</button>
+
+      {error && error.response && <div> {error.response.data} </div>}
+    </form>
   );
 };
 
