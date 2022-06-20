6c55ea70e4fa42096d614df04303a8fcb1778d7b
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-06-20 02:54:48 -0400

    feat: admin can view user info and dashboard -#54

--- client/components/AdminDashboard.js
+++ client/components/AdminDashboard.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { Statistic } from 'antd';
+import { LikeOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
 
 // Styled Components
 const StyledAdminDashboard = styled.div`
   display: flex;
   flex-direction: column;
-  height: 50vh;
+  height: 40vh;
   justify-content: space-between;
 `;
 
 const AdminDashboard = () => {
   return (
     <StyledAdminDashboard>
-      <Statistic title="Active Users" value={112893} />
-      <Statistic title="Account Balance" value={112893} precision={2} />
-      <Statistic title="Active Users" value={112893} loading />
+      <Statistic title="Active Users" value={23239} />
+      <Statistic title="Account Balance" value={18347} precision={2} />
+      <Statistic title="Feedback" value={1128} prefix={<LikeOutlined />} />
     </StyledAdminDashboard>
   );
 };
