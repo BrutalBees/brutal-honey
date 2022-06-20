142cea09b9db1e0d62f4f70c0175ec20b1f21db6
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-06-20 15:28:16 -0400

    feat: add cart route to Routes

--- client/Routes.js
+++ client/Routes.js
@@ -4,6 +4,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import SingleProduct from './components/SingleProduct';
 import Home from './components/Home';
 import AllProducts from './components/AllProducts';
+import Cart from './components/Cart';
 import AdminView from './components/AdminView';
 import Signup from './components/SignUp';
 import Login from './components/Login';
@@ -23,6 +24,7 @@ const Routes = () => {
         <Route path="/home" component={Home} />
         <Route path="/products" component={AllProducts} />
         <Route path="/product/:id" component={SingleProduct} />
+        <Route path="/cart" component={Cart} />
         {isAdmin !== false ? <Route path="/admin" component={AdminView} /> : ''}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
---
+++ client/components/Cart.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Cart = () => {
+  return(
+    <div>THIS IS THE SHOPPING CART</div>
+  )
+};
+
+export default Cart;
