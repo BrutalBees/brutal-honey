e90b7b253b06e0de6ac6d913ba17adafb20830fb
Nyjia Williams <nyjiawilliams@gmail.com>
2022-06-21 19:41:55 +0000

    merge

--- client/components/OrderConfirmation.js
+++ client/components/OrderConfirmation.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import {StyledSplash} from './styles';
+import { Card, Button } from 'react-bootstrap';
+import { StyledWrapper } from './SignUp';
+import styled from 'styled-components';
+const StyledConfirmationBox = styled.div`
+  display: flex;
+  justify-content: center;
+  border: 5px solid #b48139;
+  background-color: white;
+  height: 300px;
+  width: 350px;
+  
+`
 
 const OrderConfirmation = () => {
   return (
-    <div>
-    <StyledSplash>
-    <h1>THANK YOU!</h1>
-    <h2>We have recieved your order.</h2>
-    <h2>A confirmation has been sent to your email.</h2>
-    <h3>Your order confirmation is 5514125</h3>
-    </StyledSplash>
-    </div>
+    <StyledWrapper>
+      <StyledConfirmationBox>
+        <h1>THANK YOU!</h1>
+        <h2> We have recieved your order.</h2>
+        <p> A confirmation has been sent to your email.
+          Your order confirmation is 5514125
+        </p>
+      </StyledConfirmationBox>
+    </StyledWrapper>
+  
+  // <Card border="primary" style={{ width: '18rem' }} className="confirmation-card">
+  //   <Card.Body>
+  //     <Card.Title>THANK YOU!</Card.Title>
+  //     <Card.Text>
+  //     We have recieved your order.
+  //     </Card.Text>
+  //     <Card.Text>
+  //     A confirmation has been sent to your email.
+  //     </Card.Text>
+  //     <Card.Text>
+  //     Your order confirmation is 5514125
+  //     </Card.Text>
+  //     <Button variant="primary">Continue Shopping</Button>
+  //   </Card.Body>
+  // </Card>
   );
 };
 
