ad03622ace5269f82b9c175d43698a483c33fe67
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-06-20 21:31:14 -0400

    styling: cart page #60

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCart } from "../store/cart";
+import CartProduct from "./CartProduct";
 import styled from "styled-components";
+import { StyledProductsLink } from "./styles";
 
 // Styled Components
 const StyledCartWrapper = styled.div`
@@ -17,64 +19,10 @@ const StyledCart = styled.div`
   border-top: solid #cdc5c4 0.5px;
 `;
 
-const StyledCartRow = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-  padding: 20px;
-  border-bottom: solid #cdc5c4 0.5px;
-  height: 150px;
+const StyledCheckoutLink = styled(StyledProductsLink)`
+  align-self: flex-end;
+  margin-right: 0px;
 `;
-
-const StyledProductImage = styled.div`
-  background-image: url(${(props) => props.image});
-  background-size: cover;
-  background-position: center;
-  height: 100%;
-  width: 10%;
-`;
-
-const StyledProductName = styled.div`
-  display: flex;
-  width: 20%;
-  font-size: 15px;
-  font-weight: 800;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 0px 30px;
-`;
-
-const StyledProductPrice = styled(StyledProductName)`
-  width: 15%;
-  padding: 0px 30px;
-  justify-content: flex-end;
-  padding: 0px 30px;
-`;
-
-const StyledProductQuantity = styled.div`
-  display: flex;
-  width: 20%;
-  padding: 0px 30px;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 0px 30px;
-  border: solid grey 0.5px;
-`;
-
-const StyledQuantityInput = styled.input`
-  display: flex;
-  border: none;
-  padding: 0;
-  background-color: white;
-  margin: 0;
-`;
-
-const StyledQuantityButton = styled.button`
-  display: flex;
-  border: none;
-  background-color: white;
-`;
-
 
 // Cart Component
 const Cart = () => {
@@ -89,28 +37,13 @@ const Cart = () => {
       <h1>Your Cart</h1>
       <StyledCart>
         {products && products.map(product =>
-          <StyledCartRow key={product.id}>
-            <StyledProductImage image={product.imageUrl[0]}></StyledProductImage>
-            <StyledProductName>{product.productName}</StyledProductName>
-            <StyledProductPrice>${product.price}</StyledProductPrice>
-            <StyledProductQuantity>
-              <form>
-                <StyledQuantityButton>{"-"}</StyledQuantityButton>
-                <StyledQuantityInput
-                  type="text"
-                  value={product.cartProduct.quantity}
-                />
-                <StyledQuantityInput
-                  type="submit"
-                  name="Update"
-                />
-                <StyledQuantityButton>{"+"}</StyledQuantityButton>
-              </form>
-            </StyledProductQuantity>
-            <StyledProductPrice>${product.price * product.cartProduct.quantity}</StyledProductPrice>
-        </StyledCartRow>
+          <CartProduct
+            key={product.id}
+            product={product}
+          />
         )}
       </StyledCart>
+      <StyledCheckoutLink to="/home">CHECK OUT</StyledCheckoutLink>
     </StyledCartWrapper>
   )
 };
