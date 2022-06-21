9fe968017a6a476c3115bf326bd0adc48ddf5f2b
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-06-21 15:23:49 -0400

    style: cart quantity input

--- client/components/styles.js
+++ client/components/styles.js
@@ -229,8 +229,6 @@ const StyledProductQuantity = styled.div`
   width: 100%;
   height: 40px;
   background-color: white;
-  margin-bottom: 10px;
-  margin-top: 40px;
   border: solid grey 0.5px;
 `;
 
