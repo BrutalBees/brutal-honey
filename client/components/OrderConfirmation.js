fa20c30982f099881bbfaaa46d5edb8b55a77a44
Nyjia Williams <nyjiawilliams@gmail.com>
2022-06-21 20:38:19 +0000

    feat:Checkout page

--- client/components/OrderConfirmation.js
+++ client/components/OrderConfirmation.js
@@ -5,23 +5,28 @@ import { StyledWrapper } from './SignUp';
 import styled from 'styled-components';
 const StyledConfirmationBox = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   border: 5px solid #b48139;
-  background-color: white;
+  color: #ffffff;
   height: 300px;
   width: 350px;
-  
+  overflow: auto;
+  font-family: "Nunito", sans-serif;
+  text-align: center;
+  font-size: 150%;
+  background-color:rgba(180, 129, 57, 0.5);
 `
+
 
 const OrderConfirmation = () => {
   return (
     <StyledWrapper>
       <StyledConfirmationBox>
-        <h1>THANK YOU!</h1>
-        <h2> We have recieved your order.</h2>
-        <p> A confirmation has been sent to your email.
-          Your order confirmation is 5514125
-        </p>
+        <div>THANK YOU!</div>
+        <div> We have recieved your order.</div>
+        <div>A confirmation has been sent to your email.
+          Your order confirmation is: 5514125</div>
       </StyledConfirmationBox>
     </StyledWrapper>
   
