8939104c8208f17c15d6b9de9d9e00c62033382e
Nyjia Williams <nyjiawilliams@gmail.com>
2022-06-21 21:22:59 +0000

    changed styling/README

--- client/components/OrderConfirmation.js
+++ client/components/OrderConfirmation.js
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {StyledSplash} from './styles';
 import { Card, Button } from 'react-bootstrap';
 import { StyledWrapper } from './SignUp';
 import styled from 'styled-components';
+import { useSelector, useDispatch } from "react-redux";
+
+import { fetchCart } from "../store/cart";
+
 const StyledConfirmationBox = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  border: 5px solid #b48139;
+  border: 5px solid #ffffff;
+  padding: 20px 25px 25px 20px;
   color: #ffffff;
-  height: 300px;
-  width: 350px;
+  height: 400px;
+  width: 400px;
   overflow: auto;
   font-family: "Nunito", sans-serif;
   text-align: center;
-  font-size: 150%;
-  background-color:rgba(180, 129, 57, 0.5);
+  font-size: 180%;
+  background-color:rgba(193, 125, 31, 0.8);
 `
 
 
-const OrderConfirmation = () => {
+const OrderConfirmation = (props) => {
+  const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+   useEffect(() => { dispatch(fetchCart()) }, [dispatch]);
+   console.log(cart)
   return (
     <StyledWrapper>
       <StyledConfirmationBox>
         <div>THANK YOU!</div>
         <div> We have recieved your order.</div>
         <div>A confirmation has been sent to your email.
-          Your order confirmation is: 5514125</div>
+          Your order confirmation is:</div>
+        <div> {cart.id }</div>
       </StyledConfirmationBox>
     </StyledWrapper>
   );
