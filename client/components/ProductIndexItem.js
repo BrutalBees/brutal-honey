a75e9e94ffd108b6fc88b1b51c6edaae39577c43
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-06-21 10:33:39 -0400

    feat: links to singleProducts

--- client/components/ProductIndexItem.js
+++ client/components/ProductIndexItem.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { StyledItemWrapper, StyledItemImage, StyledProductName } from "./styles";
 
 // ProductIndexItem Component
 const ProductIndexItem = (props) => {
   const { product } = props;
   return (
-    <StyledItemWrapper>
-      <StyledItemImage image={product.imageUrl[0]} />
-      <StyledProductName>
-        {product.productName.toUpperCase()}
-      </StyledProductName>
-    </StyledItemWrapper>
+    <Link to={`/product/${product.id}`} style={{ color: "black"}}>
+      <StyledItemWrapper>
+        <StyledItemImage image={product.imageUrl[0]} />
+        <StyledProductName>
+          {product.productName.toUpperCase()}
+        </StyledProductName>
+      </StyledItemWrapper>
+    </Link>
   )
 };
 
