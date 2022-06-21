adabd2960cd4e553b60763c12a592c4d8911f115
Nyjia Williams <nyjiawilliams@gmail.com>
2022-06-21 15:28:07 +0000

    added styledsplash to checkout page

--- client/components/OrderConfirmation.js
+++ client/components/OrderConfirmation.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {StyledSplash} from './styles';
 
 const OrderConfirmation = () => {
   return (
-    <div>THANK YOU FOR YOUR ORDER</div>
+    <div>
+    <StyledSplash>
+    <h1>THANK YOU!</h1>
+    <h2>We have recieved your order.</h2>
+    <h2>A confirmation has been sent to your email.</h2>
+    <h3>Your order confirmation is 5514125</h3>
+    </StyledSplash>
+    </div>
   );
 };
 
